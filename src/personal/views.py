cdd36a10d1e76b0a84b0ecbc71388729626f3321
abe <aborg@csumb.edu>
2024-01-05 10:40:19 -0800

    initial commit

---
+++ src/personal/views.py
@@ -0,0 +1,11 @@
+from operator import attrgetter
+from django.shortcuts import render
+from account.models import Account
+from blog.models import BlogPost
+
+# Create your views here.
+def home_screen_view(request):
+    context = {}
+    blog_posts = sorted(BlogPost.objects.all(), key = attrgetter('date_updated'), reverse = True)
+    context['blog_posts'] = blog_posts
+    return render(request, 'personal/home.html', context)
