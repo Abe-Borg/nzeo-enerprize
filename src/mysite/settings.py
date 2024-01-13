cdc03fc683a2a796a51e81bb8aa1a8c26f35120c
abe <aborg@csumb.edu>
2024-01-13 08:04:56 -0800

    changed db settings, switching to postresql

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'enerprize',
+        'USER': 'abe_borg',
+        'PASSWORD': 'P+aqv&6%4GG+',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
