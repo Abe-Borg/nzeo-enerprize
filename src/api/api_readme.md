f0127401319b55d1cd201d66834a02557747127f
abe <aborg@csumb.edu>
2024-01-15 13:13:29 -0800

    adding initial code for green button api

--- src/api/api_readme.md
+++ src/api/api_readme.md
@@ -5,3 +5,6 @@ instances to python data types that can then be easily rendered into JSON.
 - in drf, views handle the logic of processing requests and returning responses. 
 there can be different types of views provided by drf, like APIView, GenericAPIView and others
 - drf integrates well with Django's authentication system and provides additional features for api keys, OAuth, etc.
+- I will take a hybrid approach, django forms and ajax calls for most things, api for others.
+- green button data will be an api call
+- 
--- src/api/views.py
+++ src/api/views.py
@@ -1,2 +1,26 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
+from .serializers import UserSerializer, GroupSerializer, AccountSerializer
+from django.http import HttpResponse
+import json
+import requests
+
+# api call for green button data
+# this api will call the green button data utility and serve to the dashboards
+
+
+def fetch_green_button_data(request):
+    # call green button data utility
+    # return json data
+    url = 'https://api.utilitycompany.com/greenbuttondata'  # placeholder url
+    params = {'param1': 'value1', 'param2': 'value2'}  # placeholder params
+
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        return HttpResponse(json.dumps(data), content_type="application/json")
+
+
+
+
+
