1c9b6291806a4677d73397687bc557714cc3476b
abe <aborg@csumb.edu>
2024-01-17 08:42:02 -0800

    removed the registration view, not needed

--- src/account/views.py
+++ src/account/views.py
@@ -10,24 +10,7 @@ from django.contrib import messages
 from .forms import AccountUpdateForm, RegistrationForm, AccountAuthenticationForm
 
 
-def registration_view(request):
-    context = {}
-    if request.POST:
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            email = form.cleaned_data.get('email')
-            raw_password = form.cleaned_data.get('password1')
-            account = authenticate(username=email, password=raw_password)
-            login(request, account)
-            return HttpResponseRedirect(reverse('enerprize_home'))
-        else:
-            context['registration_form'] = form
-    else:
-        form = RegistrationForm()
-        context['registration_form'] = form
-    return render(request, 'account/register.html', context)
-
+# registration view is handled by django registration redux
 
 def logout_view(request):
     logout(request)
