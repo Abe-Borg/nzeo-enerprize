4ee58f69d04d1b75b84f7b0fd00ee6cc52e6e228
Abey <abrahamborg@gmail.com>
2024-01-20 14:43:34 -0800

    delete apps for redo attempt

--- src/maps/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
-
-
-@login_required
-def overall_map(request, *args, **kwargs):
-    return render(request, 'overall_map.html')
