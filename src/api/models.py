ef6366038908de86771183b893c502b80303f58a
Abey <abrahamborg@gmail.com>
2024-01-20 14:41:35 -0800

    delete api app

--- src/api/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class GreenButtonData(models.Model):
-    id = models.AutoField(primary_key=True)
-    some_metric = models.CharField(max_length=20, default="Some metric")
