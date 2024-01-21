295ce8b842104097ab2e0e2a19c76b669f8eca77
Abey <abrahamborg@gmail.com>
2024-01-20 16:14:23 -0800

    district_management app base templates

--- src/district_management/views.py
+++ src/district_management/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+# @login_required
+def district_admin_home(request):
+    context = {}
+    return render(request, 'district_admin_home.html', context)
