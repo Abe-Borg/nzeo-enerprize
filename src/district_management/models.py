998cb7b913c2443830ff18c092dfe6835652f87c
Abey <abrahamborg@gmail.com>
2024-01-23 09:21:24 -0800

    edits to maps models, and district man template

--- src/district_management/models.py
+++ src/district_management/models.py
@@ -6,8 +6,10 @@ class SchoolDistrict(models.Model):
     district_num_schools = models.IntegerField()
     district_geo_lat = models.FloatField()
     district_geo_long = models.FloatField()
-    district_bounding_box_bottom_left = models.FloatField()
-    district_bounding_box_top_right = models.FloatField()
+    district_bb_bottom_left_lat = models.FloatField() # bottom left corner of bounding box, southwest corner lat, 
+    district_bb_bottom_left_lng = models.FloatField() # southwest corner lng
+    district_bb_top_right_lat = models.FloatField() # top right corner of bounding box, northeast corner lat
+    district_bb_top_right_lng = models.FloatField() # northeast corner lng
 
 
 
