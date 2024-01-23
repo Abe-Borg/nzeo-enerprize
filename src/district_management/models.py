fc896df46239687576d21719813d43b42148c93b
Abey <abrahamborg@gmail.com>
2024-01-23 07:39:34 -0800

    district_management models and postgis install

--- src/district_management/models.py
+++ src/district_management/models.py
@@ -1,11 +1,15 @@
-# from django.db import models
+from django.db import models
 
-# class SchoolDistrict(models.Model):
-#     district_id = models.IntegerField()
-#     district_name = models.CharField(max_length=100, default="District")
-#     district_num_schools = models.IntegerField()
-#     district_geo_lat = models.FloatField()
-#     district_geo_long = models.FloatField()
+class SchoolDistrict(models.Model):
+    district_id = models.IntegerField()
+    district_name = models.CharField(max_length=100, default="District")
+    district_num_schools = models.IntegerField()
+    district_geo_lat = models.FloatField()
+    district_geo_long = models.FloatField()
+    district_bounding_box_bottom_left = models.FloatField()
+    district_bounding_box_top_right = models.FloatField()
 
-#     def __str__(self):
-#         return f"District: {self.district_name}, Number of Schools: {self.district_num_schools}" 
+
+
+    def __str__(self):
+        return f"District: {self.district_name}, Number of Schools: {self.district_num_schools}" 
