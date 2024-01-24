2b03dfcb883214fe6034386391ce4c22b6bd668a
abe <aborg@csumb.edu>
2024-01-24 14:53:48 -0800

    add funcs in view to accept named loc & ret coords

--- src/district_management/views.py
+++ src/district_management/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 # from django.contrib.auth.decorators import login_required
 # from django.contrib.auth.mixins import LoginRequiredMixin
+import googlemaps
+gmaps = googlemaps.Client(key='AIzaSyANW4JtLihHDKEiBkkknOHOn6CCX-WwthA')
 
 # @login_required
 def district_admin_home(request):
@@ -26,7 +28,52 @@ def district_admin_home(request):
         'southwest_lng': -119.721710,
         'northeast_lat': 36.879697,
         'northeast_lng': -119.295908,
-        'district_name': 'Sanger Unified School District'
+        'district_name': 'Sanger Unified School District',
+        'API_KEY': 'AIzaSyANW4JtLihHDKEiBkkknOHOn6CCX-WwthA'
     }
 
+    district_schools_list = [
+        {"name": 'Centerville Elementary', "address": "48 S Smith Ave, Sanger, CA 93657"},
+        {'name': 'Del Rey Elementary', 'address': "10620 Morro Ave, Del Rey, CA 93616"},
+        {'name': 'Fairmont Elementary', 'address': "3095 N Greenwood Ave, Sanger, CA 93657"},
+        {'name': 'Jackson Elementary', 'address': "1810 3rd St, Sanger, CA 93657"},
+        {'name': 'Hallmark Academy', 'address': "2445 9th St, Sanger, CA 93657"},
+        {'name': 'Jefferson Elementary', 'address': "1110 Tucker, Sanger, CA 93657"},
+        {'name': 'Lincoln Elementary', 'address': "1700 14th St, Sanger, CA 93657"},
+        {'name': 'John Wash Elementary', 'address': "6350 E Lane Ave, Fresno, CA 93727"},
+        {'name': 'Lone Stary Elementary', 'address': "2617 S Fowler Ave, Fresno, CA 93725"},
+        {'name': 'Reagan Elementary', 'address': "1586 S Indianola Ave, Sanger, CA 93657"},
+        {'name': 'Madison Elementary', 'address': "2324 Cherry Ave, Sanger, CA 93657"},
+        {'name': 'Quail Lake Environmental Charter S chool', 'address': "4087 N Quail Lake Dr, Clovis, CA 93619"},
+        {'name': 'Sequoia', 'address': "1820 S Armstrong Ave, Fresno, CA 93727"},
+        {'name': 'Sanger Academy Charter School', 'address': "2207 9th St, Sanger, CA 93657"},
+        {'name': 'Wilson Elementary', 'address': "610 Faller Ave, Sanger, CA 93657"}
+    ]
+
+    district_schools_coordinates = get_coordinates_for_named_locations(district_schools_list)
+    context['district_schools_coordinates'] = district_schools_coordinates
+
     return render(request, 'district_management/district_admin_home.html', context)
+
+
+def get_geo_coordinates(address):
+    geocode_result = gmaps.geocode(address)
+    latitude = geocode_result[0]['geometry']['location']['lat']
+    longitude = geocode_result[0]['geometry']['location']['lng']
+    return latitude, longitude
+
+
+# This function takes a list of named locations and their addresses, 
+# geocodes the addresses, and returns a list of dictionaries with names and coordinates.
+def get_coordinates_for_named_locations(named_locations):
+    marker_data = []
+    for location in named_locations:
+        name, address = location['name'], location['address']
+        try:
+            lat, lng = get_geo_coordinates(address)
+            marker_data.append({'name': name, 'lat': lat, 'lng': lng})
+        except IndexError:
+            print(f"Geocoding failed for address: {address}")
+            continue  # Skip addresses for which geocoding fails
+    return marker_data
+
