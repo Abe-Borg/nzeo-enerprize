ec32a475aa92ef440ef45e2680bcb822c7df7095
Abey <abrahamborg@gmail.com>
2024-01-29 09:46:04 -0800

    implemented redirect after login

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+from . views import redirect_after_login
 from django.contrib.auth.views import LoginView
 from django.urls import path
 
@@ -26,13 +26,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LoginView.as_view(template_name = 'enerprize_home_page.html'), name = 'login'),
-    # path('test-base-minimal/', views.test_base_minimal, name = 'test_base_minimal'),
-    # path('test-base-nav-top-sidebar/', views.test_base_nav_top_sidebar, name = 'test_base_nav_top_sidebar'),
-    # path('test-base-side-navbar/', views.test_base_side_navbar, name = 'test_base_side_navbar'),
-    # path('test-base-top-navbar/', views.test_base_top_navbar, name = 'test_base_top_navbar'),
-    # path('test-account-settings/', views.test_account_settings, name = 'test_account_settings'),
-    # path('test-create-account/', views.test_create_account, name = 'test_create_account'),
-    # path('test-forgot-password/', views.test_forgot_password, name = 'test_forgot_password'),
+    path('redirect_after_login/', redirect_after_login, name = 'redirect_after_login'),
     path('district-management/', include('district_management.urls')),
     path('documents/', include('documents.urls')),
     path('enerprize-api/', include('enerprize_api.urls')),
--- src/mysite/views.py
+++ src/mysite/views.py
@@ -1,5 +1,16 @@
 # mysite/views.py
-from django.shortcuts import render
-from . import settings
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
+def redirect_after_login(request):
+    user = request.user
+    if user.groups.filter(name='NZEO Staff').exists():
+        return redirect('nzeo_admin_home')
+    elif user.groups.filter(name='District Admin').exists():
+        return redirect('district_admin_home')
+    elif user.groups.filter(name='School Staff').exists():
+        return redirect('school_staff_home')
+    else:
+        return redirect('enerprize_home_page')  # Fallback redirect
