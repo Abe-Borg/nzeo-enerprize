2439fb1a3a33b144c75e5613fdbd460b305ea76d
abe <aborg@csumb.edu>
2024-02-09 12:04:56 -0800

    included django debug toolbar

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', 
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -60,9 +61,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
+
+INTERNAL_IPS = [
+    # Loopback addresses
+    '127.0.0.1',
+]
 
 TEMPLATES = [
     {
