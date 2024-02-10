23bafc360c53b6e00b4e7e4e41c7710e87a0952f
abe <aborg@csumb.edu>
2024-02-10 11:21:35 -0800

    commit before manual updates to school areas.

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -89,6 +89,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
+# use to log sql queries to console
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'level': 'DEBUG',
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         'django.db.backends': {
+#             'level': 'DEBUG',
+#             'handlers': ['console'],
+#         },
+#     },
+# }
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
