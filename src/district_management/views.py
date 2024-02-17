95d1ded11dddd411c62a86f8e7fd8979f33256df
Abey <abrahamborg@gmail.com>
2024-02-17 11:18:25 -0800

    del funcs for dynamic creation of geo coordinates

--- src/district_management/views.py
+++ src/district_management/views.py
@@ -31,55 +31,6 @@ def district_admin_home(request):
     return render(request, 'district_management/district_admin_home.html', context)
 
 
-def get_geo_coordinates(address):
-    """
-    Retrieve the geographical coordinates (latitude and longitude) for a given address.
-    This function uses the Google Maps Geocoding API to convert an address into a set of 
-    geographical coordinates. It extracts the latitude and longitude from the geocoding 
-    result and returns them.
-    Parameters:
-    address (str): The address for which the geographical coordinates are to be obtained.
-    Returns:
-    tuple: A tuple containing the latitude and longitude of the given address.
-    Raises:
-    IndexError: If the geocode result is empty, indicating the address was not found.
-    """
-    geocode_result = gmaps.geocode(address)
-    latitude = geocode_result[0]['geometry']['location']['lat']
-    longitude = geocode_result[0]['geometry']['location']['lng']
-    return latitude, longitude
-
-
-def get_coordinates_for_named_locations(named_locations):
-    """
-    Get geographical coordinates for a list of named locations.
-    This function iterates through a list of named locations, each with a name and an address.
-    It uses the `get_geo_coordinates` function to retrieve latitude and longitude for each address.
-    The resulting coordinates, along with the name, are stored in a list of dictionaries. If the
-    geocoding for an address fails (due to an IndexError), it prints a message and continues with
-    the next location.
-    Parameters:
-    named_locations (list of dict): A list of dictionaries, each containing the 'name' and 'address'
-                                    of a location.
-    Returns:
-    list of dict: A list of dictionaries, each containing the 'name', 'lat' (latitude), and 'lng' 
-                  (longitude) for each successfully geocoded location.
-    Note:
-    The function will print a message for each location where geocoding fails and will not include
-    these locations in the returned list.
-    """
-    marker_data = []
-    for location in named_locations:
-        name, address = location['name'], location['address']
-        try:
-            lat, lng = get_geo_coordinates(address)
-            marker_data.append({'name': name, 'lat': lat, 'lng': lng})
-        except IndexError:
-            print(f"Geocoding failed for address: {address}")
-            continue 
-    return marker_data
-
-
 @login_required
 def school_level_analytics(request):    
     return render(request, 'district_management/school_level_analytics.html')
