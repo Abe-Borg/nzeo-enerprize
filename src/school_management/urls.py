8c5c55a03548e03f6c5655f8f60f7c6e7263f882
abe <aborg@csumb.edu>
2024-02-29 12:25:59 -0800

    started school level analytics view and template

--- src/school_management/urls.py
+++ src/school_management/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('school_home', views.school_home, name = 'school_home'),
     path('equipment-home/<int:equipment_id>/', views.equipment_home, name = 'equipment_home'),
+    path('school-analytics/<int:school_id>/', views.school_analytics, name = 'school_analytics'),
     path('add_utility_bill', views.add_utility_bill, name = 'add_utility_bill'),
     path('check_calculations', views.check_calculations, name = 'check_calculations'),
 ]
